f6a9480e4c0455cd0f0128c59776486f9a95375b
Sankung Sillah <montesillah@gmail.com>
2020-12-13 18:52:45 -0500

    initial commit

---
+++ Domain/Value.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Domain
+{
+    public class Value
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        
+        
+        
+        
+        
+    }
+}
